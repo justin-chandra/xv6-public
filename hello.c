5059c6be9aa4198f6cb56f6140242f52ef7c8aa7
Justin Chandra <jchan110@sledge.cs.ucr.edu>
2017-10-23 12:22:17 -0700

    added wait test

--- hello.c
+++ hello.c
@@ -8,9 +8,35 @@
 #include "traps.h"
 #include "memlayout.h"
 
+#define NULL 0
+
+void hello();
+void wait_example();
+
 int main(int argc, char * argv[])
+{
+    wait_example();
+    // hello();
+    return 0;
+}
+
+void hello()
 {
     int stdout = 1;
     printf(stdout, "Hello World!\n");
-    return 0;
+}
+
+void wait_example()
+{
+    int pid = 0;
+    if (fork() == 0)
+    {
+        exit(0);
+    }
+    else
+    {
+        pid = wait(NULL);
+    }
+    printf(1, "Parent pid = %d\n", getpid());
+    printf(1, "Child pid = %d\n", pid);
 }
